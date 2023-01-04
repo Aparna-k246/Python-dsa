ba8284a6b7892075e92348d6f79592ae1e38a571
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-01-04 19:01:10 +0530

    Optimal soln for ArrayEquilibriuIndex

---
+++ Array Equilibrium Index.py
@@ -0,0 +1,18 @@
+
+#Time Complexity=O(N)
+#Space Complexity=O(1)
+
+def arrayEquilibriumIndex(arr,n):
+  rightSum,leftSum=0,0
+  for i in range(n):
+    rightSum+=arr[i]
+  for i in range(n):
+    rightSum-=arr[i]
+    if rightSum==leftSum:
+      return i
+    leftSum+=arr[i]
+  return -1
+
+arr=[1, 4, 9, 3 ,2]
+n=5
+arrayEquilibriumIndex(arr,n)
