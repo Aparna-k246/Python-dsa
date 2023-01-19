be8926a5cdfd45b4c282aee373f141ecc46998b1
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-01-19 21:36:26 +0530

    Create Array Sum.py

---
+++ Array Sum.py
@@ -0,0 +1,9 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+N=int(input())
+
+li=[int(x) for x in input().split()]
+sum=0
+for ele in li:
+    sum=sum+ele
+print(sum)
