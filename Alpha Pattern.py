eda044be5393cffe6ed42386877bba0f54c10cc9
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-01-21 18:40:58 +0530

    Create Alpha Pattern.py

---
+++ Alpha Pattern.py
@@ -0,0 +1,14 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+n=int(input())
+i=1
+while i<=n:
+    j=1
+    k=ord('A')+i-1
+    while j<=i:
+        char=chr(k)
+        print(char,end='')
+        j=j+1
+        
+    i=i+1
+    print()
