bb3781f721560860b0d5d5cc37a450d066aeb84a
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-02-01 22:09:58 +0530

    Create Interesting Alphabets.py

---
+++ Interesting Alphabets.py
@@ -0,0 +1,14 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+n = int(input())
+i = 1
+while i <= n:
+    j = 1
+    startP = ord('A')+n - i
+    while j<= i:
+        #print jth column
+        
+        print(chr(startP+j-1), end='')
+        j = j + 1
+    print()
+    i = i + 1
