fd5331bda37b997adc2366b5168fe38859a140cf
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-02-19 16:09:20 +0530

    Create Check redundant Brackets.py

---
+++ Check redundant Brackets.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+def checkRedundantBrackets(expression) :
+    stack = []
+    
+    for char in expression:
+        if char==')' :
+            count = 0
+            while(stack.pop() != '('):
+                count += 1
+            if count<=1:
+                return True
+        else:
+            stack.append(char)
+            
+    return False
+
+#main
+expression = stdin.readline().strip()
+if checkRedundantBrackets(expression) :
+	print("true")
+else :
+	print("false")
