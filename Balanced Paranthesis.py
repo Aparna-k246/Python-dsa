d94780f2816c2e9e60fb5fd93298a3c669c20e3c
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-02-19 16:07:35 +0530

    Create Balanced Paranthesis.py

---
+++ Balanced Paranthesis.py
@@ -0,0 +1,36 @@
+from sys import stdin
+import queue
+
+def isBalanced(expression) :
+    stck = []
+    
+    for char in expression:
+        if char in '({[' :
+            stck.append(char)
+            
+        elif char==')':
+            if not stck or stck[-1]!='(' :
+                return False
+            stck.pop()
+            
+        elif char=='}':
+            if not stck or stck[-1]!='{' :
+                return False
+            stck.pop()
+            
+        elif char==']':
+            if not stck or stck[-1]!='[' :
+                return False
+            stck.pop()
+    
+    if not stck:
+        return True
+    
+    return False
+
+#main
+expression = stdin.readline().strip()
+if isBalanced(expression) :
+	print("true")
+else :
+	print("false")
