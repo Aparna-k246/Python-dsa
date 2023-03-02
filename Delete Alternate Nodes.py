c569c6dcc6ab18a2f1d552225653c9953bf09640
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-03-02 22:23:37 +0530

    Create Delete Alternate Nodes.py

---
+++ Delete Alternate Nodes.py
@@ -0,0 +1,18 @@
+import math
+
+class Node:
+	def _init_(self, data):
+		self.data = data
+		self.next = None
+
+def deleteAlternateNodes(head):
+	if (head == None):
+		return
+	prev = head
+	now = head.next
+	while (prev != None and now != None):
+		prev.next = now.next
+		now = None
+		prev = prev.next
+		if (prev != None):
+			now = prev.next
