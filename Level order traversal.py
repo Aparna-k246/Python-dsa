e639ce47f1cab8a603c493013fd52513dae48364
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-03-09 21:19:45 +0530

    Create Level order traversal.py

---
+++ Level order traversal.py
@@ -0,0 +1,69 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+        
+def printLevelWise(root):
+    if root is None:
+        return None
+    
+    q = queue.Queue()
+    q.put(root)
+    q.put(None)
+    
+    while(not q.empty()):
+        currentNode = q.get()
+        
+        if currentNode is None:
+            print()
+            if(not q.empty()):
+            	q.put(None)
+                 
+        else:
+            print(currentNode.data, end=' ')
+            
+            if currentNode.left!=None:
+                q.put(currentNode.left)
+                
+            if currentNode.right!=None:
+                q.put(currentNode.right)                    
+    return
+                    
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    length = len(levelOrder)
+    if length == 1 :
+        return None
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelOrder[start]
+        start += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelOrder[start]
+        start += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+root = takeInput()
+printLevelWise(root)
