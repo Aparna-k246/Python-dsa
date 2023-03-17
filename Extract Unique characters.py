40abd9d7cb35ab9fa377c81926f5f9a868b72b11
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-03-17 23:38:13 +0530

    Create Extract Unique characters.py

---
+++ Extract Unique characters.py
@@ -0,0 +1,25 @@
+
+
+      
+    
+
+
+
+
+
+
+
+
+
+def uniqueChar(s): 
+    ans = '' 
+    d = {} 
+    for char in s: 
+        if char not in d: 
+            ans = ans + char 
+            d[char] = True 
+    return ans 
+
+# Main 
+s=input() 
+print(uniqueChar(s))
