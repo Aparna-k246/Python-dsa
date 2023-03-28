5903770046263b357c89769ab5c9b4dd710ba83b
Aparna-k246 <54439853+Aparna-k246@users.noreply.github.com>
2023-03-28 23:57:57 +0530

    Update Longest increasing subsequence.py

--- Longest increasing subsequence.py
+++ Longest increasing subsequence.py
@@ -1,27 +1,5 @@
-print(countMinStepsToOne(n))
-Minimum Number Of Squares
-import sys
-def minStepsTo1(n):
-    
-    dp = [0 for i in range(n+1)]
-    
-    for i in range(1,n+1):
-        dp[i] = sys.maxsize
-        for j in range(1,i+1):
-            temp = j*j
-            if temp > i:
-                break
-            dp[i] = min(dp[i],1+dp[i-temp])
-    
-    ans = dp[n]
-    return ans
-        
 
-    
-n = int(input())
-ans = minStepsTo1(n)
-print(ans)
-Longest Increasing Subsequence
+
 from sys import stdin
 def lis(arr): 
     storage = [None for i in range(n)]
