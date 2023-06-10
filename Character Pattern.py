c51764b214e0125696ab87f39fa5b2873f2e0a6a
Aparna K <54439853+Aparna-k246@users.noreply.github.com>
2023-06-10 23:04:26 +0530

    Update Character Pattern.py

--- Character Pattern.py
+++ Character Pattern.py
@@ -7,6 +7,7 @@ while i <= n:
     j = 1
     startP = chr(ord('A') + i - 1)
     while j<= i:
+        
         #print jth column
         charP = chr(ord(startP) + j - 1)
         print(charP, end='')
