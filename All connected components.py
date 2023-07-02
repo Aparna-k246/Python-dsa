fddf3ecbd214d92ffd6e5b276f3f739d603f3d72
Aparna K <54439853+Aparna-k246@users.noreply.github.com>
2023-07-02 14:49:10 +0530

    Update All connected components.py

--- All connected components.py
+++ All connected components.py
@@ -6,7 +6,6 @@ class Graph:
     def __init__(self, nVertices):
         self.nVertices = nVertices
         self.adjMatrix = [[0 for i in range(nVertices)] for j in range(nVertices)]
-    
     def addEdge(self, v1, v2):
         self.adjMatrix[v1][v2] = 1
         self.adjMatrix[v2][v1] = 1
