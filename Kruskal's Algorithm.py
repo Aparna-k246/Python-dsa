e9677eaa748a26b52bbd3c24d60133622881db24
Aparna K <54439853+Aparna-k246@users.noreply.github.com>
2023-07-14 22:11:15 +0530

    Update Kruskal's Algorithm.py

--- Kruskal's Algorithm.py
+++ Kruskal's Algorithm.py
@@ -1,4 +1,4 @@
-## Read input as specified in the question.
+## Read input as specified in the questio
 ## Print output as specified in the question.
 class Edge:
     def __init__(self,src,dest,wt):
