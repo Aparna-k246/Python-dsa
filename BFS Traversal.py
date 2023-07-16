2d5b3d3aa83f0b4b61d0205676b304c95a72cebe
Aparna K <54439853+Aparna-k246@users.noreply.github.com>
2023-07-16 22:41:48 +0530

    Update BFS Traversal.py

--- BFS Traversal.py
+++ BFS Traversal.py
@@ -13,6 +13,7 @@ class Graph:
         self.adjMatrix[v2][v1] = 1
     
     def removeEdge(self):
+        
         if self.containsEdge(v1, v2) is False :
             return
         self.adjMatrix[v1][v2] = 0
