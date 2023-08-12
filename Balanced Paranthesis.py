29d236d8a21d29a89fe856d2b676ec46ae4c0099
Aparna K <54439853+Aparna-k246@users.noreply.github.com>
2023-08-12 22:31:08 +0530

    Update Balanced Paranthesis.py

--- Balanced Paranthesis.py
+++ Balanced Paranthesis.py
@@ -24,6 +24,7 @@ def isBalanced(expression) :
             
         elif char=='}':
             if not stck or stck[-1]!='{' :
+		    
                 return False
             stck.pop()
             
